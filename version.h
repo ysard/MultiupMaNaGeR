e0436999870d8a705b9dcc17d420f6e2237f04b7
ysard <ysard@users.noreply.github.com>
2020-04-29 22:02:34 +0200

    Bump version 0.4.5; Update copyright

--- version.h
+++ version.h
@@ -2,16 +2,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,4,2,0
-#define VER_FILEVERSION_STR         "0.4.2.0\0"
+#define VER_FILEVERSION             0,4,5,0
+#define VER_FILEVERSION_STR         "0.4.5.0\0"
 
-#define VER_PRODUCTVERSION          0,4,2,0
-#define VER_PRODUCTVERSION_STR      "0.4\2"
+#define VER_PRODUCTVERSION          0,4,5,0
+#define VER_PRODUCTVERSION_STR      "0.4\5"
 
 #define VER_COMPANYNAME_STR         "Multiup.org"
 #define VER_FILEDESCRIPTION_STR     "Multiup MaNaGeR"
 #define VER_INTERNALNAME_STR        "Multiup MaNaGeR"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2012-2015 Multiup.org Lex & Lunikoff"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2012-2020 Multiup.org Lex & Lunikoff"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "multiupv2.exe"
